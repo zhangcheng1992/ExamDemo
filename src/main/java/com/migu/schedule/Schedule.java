9eeb238d247e2a832b619d4fc4d7bb93052a0325
zhangcheng <15655993167@139.com>
2018-06-21 17:28:09 +0800

    1111

--- src/main/java/com/migu/schedule/Schedule.java
+++ src/main/java/com/migu/schedule/Schedule.java
@@ -3,8 +3,12 @@ package com.migu.schedule;
 
 import com.migu.schedule.constants.ReturnCodeKeys;
 import com.migu.schedule.info.TaskInfo;
+import com.migu.schedule.info.TaskInfo_zc;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /*
 *类名和方法不能修改
@@ -12,44 +16,113 @@ import java.util.List;
 public class Schedule {
 
 
+    Map<Integer,TaskInfo_zc> tasks = new ConcurrentHashMap<Integer,TaskInfo_zc>();
+
+    //    private HashSet<Integer> nodeSet = new HashSet<Integer>();
+    //当前结点的
+    Map<Integer,Map<Integer,TaskInfo_zc>> nodeTaskMap = new ConcurrentHashMap<Integer,Map<Integer,TaskInfo_zc>>();
+
     public int init() {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+        //重置结点
+//        nodeSet.clear();
+        nodeTaskMap.clear();
+        return ReturnCodeKeys.E001;
     }
 
 
     public int registerNode(int nodeId) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+        //结点非法
+        if(nodeId < 1)
+        {
+            return ReturnCodeKeys.E004;
+        }
+        //结点存在
+        if(nodeTaskMap.keySet().contains(nodeId))
+        {
+            return ReturnCodeKeys.E005;
+        }
+//        nodeSet.add(nodeId);
+        nodeTaskMap.put(nodeId,new HashMap<Integer, TaskInfo_zc>());
+
+        return ReturnCodeKeys.E003;
     }
 
     public int unregisterNode(int nodeId) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+//        nodeSet.remove(nodeId);
+
+        Map<Integer,TaskInfo_zc> task = nodeTaskMap.get(nodeId);
+        //将
+        if(null != task)
+        {
+            tasks.putAll(task);
+            nodeTaskMap.remove(nodeId);
+            return ReturnCodeKeys.E006;
+        }
+        else
+        {
+            return ReturnCodeKeys.E007;
+        }
+
     }
 
 
     public int addTask(int taskId, int consumption) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+        if(taskId < 1)
+        {
+            return ReturnCodeKeys.E009;
+        }
+        if(tasks.containsKey(taskId))
+        {
+            return ReturnCodeKeys.E010;
+        }
+        for(Integer nodeid : nodeTaskMap.keySet())
+        {
+            if (nodeTaskMap.get(nodeid).containsKey(taskId))
+            {
+                return ReturnCodeKeys.E010;
+            }
+
+        }
+        //添加到任务队列
+        tasks.put(taskId,new TaskInfo_zc(taskId,consumption));
+
+        return ReturnCodeKeys.E008;
     }
 
 
     public int deleteTask(int taskId) {
+        boolean flag = false;
         // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+        if(tasks.containsKey(taskId))
+        {
+            flag = true;
+            tasks.remove(taskId);
+        }
+        for(Integer nodeid : nodeTaskMap.keySet())
+        {
+            if (nodeTaskMap.get(nodeid).containsKey(taskId))
+            {
+                flag = true;
+                nodeTaskMap.get(nodeid).remove(taskId);
+            }
+        }
+
+        if(!flag)
+        {
+            return ReturnCodeKeys.E012;
+        }
+
+        return ReturnCodeKeys.E011;
     }
 
 
     public int scheduleTask(int threshold) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+        return ReturnCodeKeys.E013;
     }
 
 
     public int queryTaskStatus(List<TaskInfo> tasks) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
+        return ReturnCodeKeys.E015;
     }
 
 }
